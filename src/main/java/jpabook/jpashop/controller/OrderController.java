09c4d982a3eab1581550578e4b3056f7f6a7b945
hshan <hhsung0120@naver.com>
2022-07-28 23:22:25 +0900

    2022-07-28 웹어플리케이션 1편 완료

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -2,12 +2,13 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -29,5 +30,26 @@ public class OrderController {
         return new ModelAndView("/order/orderForm")
                 .addObject("members", members)
                 .addObject("items", items);
+    }
+
+    @PostMapping("/order")
+    public ModelAndView order(@RequestParam("memberId") Long memberId
+            , @RequestParam("itemId") Long itemId
+            , @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return new ModelAndView("redirect:/orders");
+    }
+
+    @GetMapping("/orders")
+    public ModelAndView orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch) {
+        return new ModelAndView("/order/orderList")
+                .addObject("orders", orderService.findOrders(orderSearch));
+    }
+
+    @PostMapping("/orders/{id}/cancel")
+    public ModelAndView cancle(@PathVariable("id") Long id) {
+        orderService.cancelOrder(id);
+        return new ModelAndView("redirect:/orders");
     }
 }
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -22,11 +22,34 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch){
-        return em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status " +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
+
+        if(orderSearch.getMemberName() == null && orderSearch.getOrderStatus() != null){
+            return em.createQuery("select o from Order o join o.member m" +
+                            " where o.status = :status " , Order.class)
+                    .setParameter("status", orderSearch.getOrderStatus())
+                    .setMaxResults(1000)
+                    .getResultList();
+        }
+
+        if(orderSearch.getMemberName() != null && orderSearch.getOrderStatus() == null){
+            return em.createQuery("select o from Order o join o.member m" +
+                            " where m.name like :name ", Order.class)
+                    .setParameter("name", orderSearch.getMemberName())
+                    .setMaxResults(1000)
+                    .getResultList();
+        }
+
+        if(orderSearch.getMemberName() != null && orderSearch.getOrderStatus() != null){
+            return em.createQuery("select o from Order o join o.member m" +
+                            " where o.status = :status " +
+                            " and m.name like :name", Order.class)
+                    .setParameter("status", orderSearch.getOrderStatus())
+                    .setParameter("name", orderSearch.getMemberName())
+                    .setMaxResults(1000)
+                    .getResultList();
+        }
+
+        return em.createQuery("select o from Order o join o.member m", Order.class)
                 .setMaxResults(1000)
                 .getResultList();
     }
