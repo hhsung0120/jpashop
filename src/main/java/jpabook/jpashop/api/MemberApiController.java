ed3157802f6511689f302f92ea0117e357afca17
hshan <hhsung0120@naver.com>
2022-10-09 23:54:03 +0900

    2022-10-08 채팅 UI 잠시 보관 및 애플리케이션 2편 공부

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,8 +7,11 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -16,6 +19,40 @@ import javax.validation.Valid;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/test/test")
+    public ModelAndView test() {
+        return new ModelAndView("/items/test");
+    }
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(4, collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -37,7 +74,7 @@ public class MemberApiController {
 
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
-              @PathVariable("id") Long id
+            @PathVariable("id") Long id
             , @RequestBody @Valid UpdateMemberRequest request) {
 
         log.info(request.toString());
